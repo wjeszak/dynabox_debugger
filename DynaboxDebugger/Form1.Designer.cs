93910dbdb2913b635979a2c3fc562fea90813228
wjeszak <wjeszak@gmail.com>
2018-01-03 23:19:27 +0100

    Dodaj pliki projektów.

---
+++ DynaboxDebugger/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DynaboxDebugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serial = new System.IO.Ports.SerialPort(this.components);
+            this.ports = new System.Windows.Forms.ComboBox();
+            this.button_connection = new System.Windows.Forms.Button();
+            this.preview = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // serial
+            // 
+            this.serial.BaudRate = 19200;
+            this.serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serial_DataReceived);
+            // 
+            // ports
+            // 
+            this.ports.FormattingEnabled = true;
+            this.ports.Location = new System.Drawing.Point(12, 12);
+            this.ports.Name = "ports";
+            this.ports.Size = new System.Drawing.Size(121, 21);
+            this.ports.TabIndex = 1;
+            // 
+            // button_connection
+            // 
+            this.button_connection.Location = new System.Drawing.Point(139, 10);
+            this.button_connection.Name = "button_connection";
+            this.button_connection.Size = new System.Drawing.Size(75, 23);
+            this.button_connection.TabIndex = 2;
+            this.button_connection.Text = "Connect";
+            this.button_connection.UseVisualStyleBackColor = true;
+            this.button_connection.Click += new System.EventHandler(this.button_connection_Click);
+            // 
+            // preview
+            // 
+            this.preview.Location = new System.Drawing.Point(247, 10);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(354, 222);
+            this.preview.TabIndex = 3;
+            this.preview.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 256);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.button_connection);
+            this.Controls.Add(this.ports);
+            this.Name = "Form1";
+            this.Text = "Dynabox Debugger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serial;
+        private System.Windows.Forms.ComboBox ports;
+        private System.Windows.Forms.Button button_connection;
+        private System.Windows.Forms.RichTextBox preview;
+    }
+}
+
---
+++ DynaboxDebugger/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace DynaboxDebugger
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(byte[] f);
+        int desired_data = 5;
+        //int desired_data = 6;
+        int received_data = 0;
+        int position = 0;
+        byte[] rxbytearray = new byte[5];
+        //byte[] rxbytearray = new byte[6];
+        enum PortStatus { Connected, Disconnected, Busy };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PortStatusInfo(PortStatus status)
+        {
+            if (status == PortStatus.Connected)
+            {
+                button_connection.Text = "Disconnect";
+            }
+            if (status == PortStatus.Disconnected)
+            {
+                button_connection.Text = "Connect";
+            }
+            if (status == PortStatus.Busy)
+            {
+                button_connection.Text = "Connect";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                ports.Items.Add(s);
+            }
+        }
+
+        private void serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int numbytes = serial.BytesToRead;
+
+            received_data += numbytes;
+            if (received_data <= desired_data)
+            {
+                for (int i = position; i < received_data; i++)
+                {
+                    rxbytearray[i] = (byte)serial.ReadByte();
+                }
+                position += numbytes;
+            }
+
+            if (received_data == desired_data)
+            {
+                received_data = 0;
+                position = 0;
+                this.BeginInvoke(new SetTextCallback(SetText), new object[] { rxbytearray });
+            }
+
+        }
+
+        private void PiszLog(string r)
+        {
+            try
+            {
+                FileStream wFile;
+                byte[] byteData = null;
+                string czas = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss fff ");
+                string ramka = czas + r;
+
+                byteData = Encoding.ASCII.GetBytes(ramka);
+                wFile = new FileStream(@"log.txt", FileMode.Append);
+                wFile.Write(byteData, 0, byteData.Length);
+                wFile.Close();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void SetText(byte[] f)
+        {
+            preview.SelectionColor = System.Drawing.Color.Red;
+            string pos = (f[0] << 8 | f[1]).ToString() + ';' + f[2].ToString() + ';' + (f[3] << 8 | f[4]).ToString() + "\r\n";
+            //string dane = f[0].ToString("X2") + ' ' + f[1].ToString("X2") + ' ' + f[2].ToString("X2") + ' ' + f[3].ToString("X2") + "\r\n";
+            preview.AppendText(pos);
+            preview.ScrollToCaret();
+            PiszLog(pos);
+        }
+
+        private void button_connection_Click(object sender, EventArgs e)
+        {
+            if (ports.SelectedItem == null) return;
+            if (serial.IsOpen == false)
+            {
+                serial.PortName = ports.SelectedItem.ToString();
+                try
+                {
+                    serial.Open();
+                }
+                catch (Exception ex)
+                {
+                    //PortStatusInfo(PortStatus.Busy);
+                }
+
+                if (serial.IsOpen == true) PortStatusInfo(PortStatus.Connected);
+            }
+            else
+            {
+                serial.Close();
+                PortStatusInfo(PortStatus.Disconnected);
+            }
+        }
+    }
+}
